707ff2c652e9f6a4849d28b3f528de1d72e83b78
Amir <Amir93@yandex.ru>
2024-12-31 02:53:02 +0100

    no.3: widget.py updated

--- src/widget.py
+++ src/widget.py
@@ -8,3 +8,9 @@ def mask_account_card(typed_card_number: str) -> str:
         return f"{typed_card_number[:-21]} {m.get_mask_account(typed_card_number[-20:])}"
     else:
         return f"{typed_card_number[:-17]} {m.get_mask_card_number(typed_card_number[-16:])}"
+
+
+def get_date(date: str) -> str:
+    """Функция возвращает строку с датой в формате ДД.ММ.ГГГГ"""
+
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
