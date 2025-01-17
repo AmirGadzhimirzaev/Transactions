bf87d0c0c8b4f33b2161d4c98190de42022cdb10
Amir <Amir93@yandex.ru>
2025-01-17 23:11:19 +0100

    no.6: modified processing.py -> new function sorted_by_state

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,9 @@ def filter_by_state(list_of_dict: list[dict], state: str = "EXECUTED") -> list[d
     """
 
     return list(filter(lambda x: x["state"] == state, list_of_dict))
+
+
+def sorted_by_state(list_of_dict: list[dict], in_descending_order: bool = True) -> list[dict]:
+    """Функция возвращает новый список словарей отсортированных по дате."""
+
+    return sorted(list_of_dict, key=lambda x: x["date"], reverse=in_descending_order)
