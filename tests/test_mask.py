d63149d1e1a509ab7bcd2eef83f5e54fcb051d34
Amir <Amir93@yandex.ru>
2025-01-27 18:10:09 +0100

    no.9: added [htmlcov/*, test_mask.py, test_widget.py, test_processing.py]; modified: [src/*, .gitignore, poetry.lock, pyproject.toml, README.md]

---
+++ tests/test_mask.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.mask import get_mask_card_number, get_mask_account
+
+
+@pytest.mark.parametrize("card_number, expected", [
+    (None, "Номер карты не должен быть None!"),
+    ("7000792289606361", "7000 79** **** 6361"),
+    ("70007922896063612", "Номер карты должен содержать 16 цифр!"),
+    ("700079228960636", "Номер карты должен содержать 16 цифр!"),
+    ("", "Номер карты должен содержать 16 цифр!"),
+    ("7AC079228960EZ61", "Номер карты должен содержать только цифры!"),
+])
+def test_get_card_number(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
+
+
+@pytest.mark.parametrize("account_number, expected", [
+    (None, "Номер аккаунта не должен быть None!"),
+    ("87564345353999334531", "**4531"),
+    ("875643453539993343123", "Номер аккаунта должен содержать 20 цифр!"),
+    ("8756434535399933431", "Номер аккаунта должен содержать 20 цифр!"),
+    ("", "Номер аккаунта должен содержать 20 цифр!"),
+    ("31212LDLF1334134DE32", "Номер аккаунта должен содержать только цифры!"),
+])
+def test_get_mask_account(account_number, expected):
+    assert get_mask_account(account_number) == expected
