545c158946bb20c57d64689e861df14934df4631
Amir <Amir93@yandex.ru>
2025-03-09 13:30:28 +0300

    no.16: fixes [htmlcov/*, src/decorators.py, tests/*]; added [tests/test_decorators.py]

--- src/decorators.py
+++ src/decorators.py
@@ -1,22 +1,27 @@
 from functools import wraps
+from typing import Any, Callable
 
 
-def log(filename=""):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            try:
-                func(*args, **kwargs)
-            except Exception as e:
-                message = f"{func.__name__} {e}. Inputs: {args}, {kwargs}"
-            else:
-                message = f"{func.__name__} ok"
+def log(filename: str = "") -> Callable:
+    """Декоратор для автоматической регистрации информации о выполнение функций"""
 
-            if filename == "" :
+    def decorator(func: Callable) -> Callable:
+        def place_for_message(message: str) -> None:
+            if filename == "":
                 print(message)
             else:
                 with open(filename, "a", encoding="utf-8") as file:
                     file.write(message + "\n")
+
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                func(*args, **kwargs)
+            except Exception as e:
+                place_for_message(f"{func.__name__} {e}. Inputs: {args}, {kwargs}")
+            else:
+                place_for_message(f"{func.__name__} ok")
+                return func(*args, **kwargs)
 
         return wrapper
 
