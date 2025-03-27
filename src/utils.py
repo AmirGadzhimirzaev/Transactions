a966d0bfd6148ae648c77a58fc66d71a5a4c38fc
Amir <Amir93@yandex.ru>
2025-03-27 11:51:56 +0300

    no.18: added logger for utils.py, mask.py

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,16 @@
 import json
+import logging
 import os
 from typing import Generator
 
-print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+utils_log_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs", "utils_logger.log")
+
+utils_logger = logging.getLogger("utils_logger")
+utils_file_handler = logging.FileHandler(utils_log_path)
+utils_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+utils_file_handler.setFormatter(utils_formatter)
+utils_logger.addHandler(utils_file_handler)
+utils_logger.setLevel(logging.DEBUG)
 
 
 def get_json_trans_list(
@@ -18,8 +26,12 @@ def get_json_trans_list(
             if len(data) == 0 or type(data) is not list:
                 raise Exception
 
-    except Exception:
+    except Exception as ex:
+        utils_logger.error(f"ERROR: {ex}")
+
         yield []
     else:
+        utils_logger.debug("DEBUG: success")
+
         for trans in data:
             yield trans
